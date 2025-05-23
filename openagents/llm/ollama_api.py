95aae8c2cf9695b24e8cc8ae2c21171bb780276d
HanishDhanwalkar <hanishpd1729@gmail.com>
2025-05-23 23:11:06 +0530

    api fixes

--- openagents/llm/ollama_api.py
+++ openagents/llm/ollama_api.py
@@ -1,41 +1,50 @@
 from ollama import Client
 
-client = Client(
-    host='http://localhost:11434',
-    headers={'x-some-header': 'some-value'}
-)
+MODEL = "llama3.2"
+# MODEL = "deepseek-r1:7b" 
 
-model = "llama3.2"
-# model = "deepseek-r1:7b" 
+STREAM = True
 
-prompt = "Choose a number between 1 and 10. No thinking, just respond with the number."
+def input_to_llm(prompt):
+    data = {
+        "model": MODEL,
+        "messages": [
+            {
+                "role": "user",
+                "content": f"{prompt}",
+            },
+        ] 
+    }
+    return data
 
-stream = True 
-
-data = {
-    "model": model,
-    "messages": [
-        {
-            "role": "user",
-            "content": f"{prompt}",
-        },
-    ] 
-}
-
-if stream:
-    stream_response = client.chat(
-        **data,
-        stream=True,
+def main():
+    client = Client(
+        host='http://localhost:11434',
+        headers={'x-some-header': 'some-value'}
     )
-    for s in stream_response:
-        print(s.get("message", {}).get("content", ""), end="")
-  
-else:
-    response = client.chat(**data)
-    print(response.get("message", {}).get("content", ""))
+
+    prompt = input("Enter a prompt: (press enter for deafault prompt) ")
+    if prompt == "":
+        prompt = "Choose a number between 1 and 10. No thinking, just respond with the number."
+
+    data = input_to_llm(prompt)
+
+    if STREAM:
+        stream_response = client.chat(
+            **data,
+            stream=True,
+        )
+        for s in stream_response:
+            print(s.get("message", {}).get("content", ""), end="")
     
-    
-    
+    else:
+        response = client.chat(**data)
+        print(response.get("message", {}).get("content", ""))
+        
+if __name__ == "__main__":
+    main()
+        
+        
 # curl http://localhost:11434/api/generate -d '{
 #   "model": "deepseek-r1:7b",
 #   "prompt": "Why is the sky blue?",
