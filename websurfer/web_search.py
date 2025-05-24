02df7f1087985d3c4eba05385318048abf057b52
HanishDhanwalkar <hanishpd1729@gmail.com>
2025-05-24 23:50:57 +0530

    .

--- websurfer/web_search.py
+++ websurfer/web_search.py
@@ -1,11 +1,45 @@
-from base import navigate2url
+# from base import navigate2url
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+
+import time
+
+def navigate2url(url, headless=False, timeout=0):
+    options = Options()
+    options.headless = headless
+    try:
+        driver = webdriver.Firefox(options=options)
+        driver.get(url)
+        # # Perform some actions on the webpage...
+        page_source = driver.page_source
+        print(driver.title) 
+        # print(page_source)
+        return driver
+    
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        time.sleep(timeout)
+        driver.quit()
+
 
 def search_duckduckgo(query):
     url = f"https://duckduckgo.com/?q={query}"
-    driver = navigate2url(url)
+    try:
+        driver = navigate2url(url)
     
-    # print(driver.title)
-    print(driver)
+        # print(driver.title)
+        # print(driver)
+        links = driver.find_elements(By.CLASS_NAME, "a")
+        print(driver.title)
+        print(links)
+        return links
+
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        driver.quit()
     
 def search_google(query):
     url = f"https://google.com/?q={query}"
@@ -15,4 +49,9 @@ def search_google(query):
     
 if __name__ == "__main__":
     query = "What's the latest news about OpenAI's new models?"
-    search_duckduckgo(query)
+    links = search_duckduckgo(query)
+    
+    
+    
+    for link in links:
+        print(link.text)
